b2976b0421bddb5c24f373eb0817e0bb9d1ea5d7
K.Himeno <k.himeno314@gmail.com>
2022-04-21 16:13:29 +0900

    Initial commit

---
+++ scripts/build.ts
@@ -0,0 +1,19 @@
+import "./clean";
+import { shell } from "./tools/shell";
+import { copyPackageSet } from "./tools/copyPackageSet";
+
+const main = async () => {
+  await Promise.all([
+    shell("pnpm tsc -p tsconfig.esm.json -d --emitDeclarationOnly --outDir ./lib/\\$types"),
+    shell("pnpm tsc -p tsconfig.cjs.json"),
+    shell("pnpm tsc -p tsconfig.esm.json"),
+  ]);
+
+  await shell("cherry-pick --cwd ./lib --input-dir ../src --types-dir ./\\$types --cjs-dir ./\\$cjs --esm-dir ./\\$esm");
+  await copyPackageSet();
+};
+
+main().catch(error => {
+  console.error(error);
+  process.exit(1);
+});
